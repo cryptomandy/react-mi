895feda960b29c9d40be26ed4fb6bb55dc662517
庾敏 <ym10862@ly.com>
2018-06-13 14:52:41 +0800

    update06-13

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,38 +1,81 @@
 import React, { Component } from 'react';
 import './home.less';
 
+
 class Base extends Component {
+  //class中的方法类似function函数，函数之间没有逗号，对象之后是等号；
+  pageData = {
+    a:1,
+    //模板显示 ,每个标签需要唯一key值，否则会有警告
+    arr:[
+      <span key='1'>第一个模板</span>,
+      <span key='2'>第二个模板</span>
+    ],
+    list:[{
+      "name":"111",
+      "id":1,
+    },{
+      "name":"222",
+      "id":2,
+    },{
+      "name":"333",
+      "id":3,
+    },{
+      "name":"444",
+      "id":4,
+      "type":1,
+    },],
+
+    createList:[]
+  }
+
+  componentWillMount(){
+    let arr = [];
+    for(let i = 0; i<100 ;i++){
+      arr.push(i);
+    }
+    // filter以某个条件过滤到需要的项，map对当前数据项进行处理，两个都可以返回；
+    this.pageData.createList.push(
+      arr.filter((item,index) => {
+        return item % 8 === 0;
+      })
+    )
+    console.log(this.pageData.createList)
+  }
   render() {
+    // https://blog.csdn.net/wmzy1067111110/article/details/51538241  react jsx中判断语句写法
     return (
       <div className="page_bg">
-        <section>
-          <h6>关于我们</h6>
-          <article>
-            小马众包是人工智能数据处理、发布、分享和交换平台，拥有专业强大的图形图像、视频跟踪、雷达点云等数据处理工具和协同工作平台，以DaaS模式为人工智能企业提供高质量、低成本的训练数据
-          </article>
-        </section>
-
-        <section>
-          <h6>隐私声明</h6>
-          <article>
-            码图尊重并保护您通过我们的平台服务（包括平台、产品服务、解决方案、数据合作等）向我们提供的所有隐私。本隐私政策（“隐私政策”）适用于通过本平台服务收集的所有个人信息，描述了我们收集、使用、披露、保护信息的方式，并为您提供访问我们所维护的关于您的个人信息的渠道。本隐私政策并不适用于您通过其他方式向我们提供的或者我们通过其他公共来源所收集的属于公开信息的个人信息。
-          </article>
-        </section>
-
-        <section>
-        <h6>常见问题</h6>
-          <h6>申请提现后，多久能到账？</h6>
-          <article>
-            一般来说，申请提现后1小时内即可到账，如果遇到第三方支付结算延期的情况，也会在1个工作日内提现到账。
-          </article>
-          <h6>如何邀请好友加入？</h6>
-          <article>
-            将自己的要求海报发送至朋友圈，或直接通过微信发送给好友，好友注册后，您可得到相应奖励。
-          </article>
-        </section>
+        <p>{11+"桔子酒店"}</p>
+        {/* render模板中用定义的数据需要加this（TODO:和vue不一致） */}
+        {/* 不能使用if-else，表达式语句 */}
+        <p>{ this.pageData.a ? 6: 7 }</p>
+        <p>{ this.pageData.arr }</p>
+        <ul>{
+          // forEach不行？？？
+          //注意key，循环过程中多个判断处理方法
+          this.pageData.list.map((item,index) => {
+              return <li key={index} className={`item_${index}`}>{item.id}-{item.name}</li>
+          })
+        }</ul>
+        {/* React DOM结点使用骆驼拼写法给属性命名 */}
+        <span className='num_list' tabIndex="0">{
+            this.pageData.createList[0].map((item,index) => {
+              return <em key={index}>{item}</em>
+            })
+        }</span>
+        {/* 多级判断语句 */}
+        
       </div>
     );
   }
 }
 
 export default Base;
+/**
+ * @name README
+ * 常用的表达式有：变量名；函数定义表达式；属性访问表达式；函数调用表达式；算数表达式；关系表达式；逻辑表达式；
+ * 
+ * */ 
+
+//  https://blog.csdn.net/a153375250/article/details/53434299 jsx语法
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,21 @@ import { HashRouter, Switch, Route } from 'react-router-dom'; // ???
 
 //页面
 import home from "../pages/home/home";
-
+import about from "../pages/member/about/about";
+/**
+ * @name react-router说明
+ * @summary 
+ * 
+ * */ 
+//http://www.ruanyifeng.com/blog/2016/05/react_router.html?utm_source=tool.lu 
+//https://www.cnblogs.com/zhanghuiming/p/7592132.html  routerV4
 export default class RouteConfig extends Component{
     render(){
         return (
             <HashRouter>
                 <Switch>
                     <Route path="/" exact component={home} />
+                    <Route path="/about" component={about} />
                 </Switch>
             </HashRouter>
         )
